7bf637c645dd3f09d7f297f9511a7720a921c59c
dnamrata7 <dbnamrata7@gmail.com>
2019-11-23 23:57:47 -0800

    updating

--- TransactionManagement/src/main/java/com/banking/beans/DBLoader.java
+++ TransactionManagement/src/main/java/com/banking/beans/DBLoader.java
@@ -18,10 +18,7 @@ public class DBLoader implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		// TODO Auto-generated method stub
-		java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());
-		Transaction transaction = new Transaction(00001,1,"Credit","Online","Refund settlement "
-				,20.00,"USD",sqlDate,40.00,"Successful");
-		transactions.save(transaction);
+	
 	}
 
 }
