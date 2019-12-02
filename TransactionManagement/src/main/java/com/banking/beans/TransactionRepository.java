0c6e3acdb63f8c8337ee47a13789efa9f200313a
dnamrata7 <dbnamrata7@gmail.com>
2019-12-02 14:25:46 -0800

    Query modification

--- TransactionManagement/src/main/java/com/banking/beans/TransactionRepository.java
+++ TransactionManagement/src/main/java/com/banking/beans/TransactionRepository.java
@@ -11,7 +11,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long>{
 	
-@Query("FROM Transaction WHERE createDt <= :currentDt AND createDt >= :minDt ORDER BY createDt DESC")
-List<Transaction> findByDt(@Param("currentDt")Date currentDt,@Param("minDt") Date minDt);
+@Query("FROM Transaction WHERE accNum = :accNum  AND createDt <= :currentDt AND createDt >= :minDt ORDER BY createDt DESC")
+List<Transaction> findByDt(@Param("accNum") long accNum, @Param("currentDt")Date currentDt,@Param("minDt") Date minDt);
 
 }
